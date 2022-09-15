7ed1056de786a3976b474078f3b5eec297b632de
ai-mayurpatil <ai-mayurpatil@gmail.com>
2022-09-15 10:54:07 +0530

    Add valueus all slider

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ dataset_name = st.sidebar.selectbox("Select Dataset",("Iris", "Breast Cancer", "
 
 classifier_name = st.sidebar.selectbox("Select Classifier",("KNN", "SVM", "Random Forest"))
 
+# Add Slide
 def get_dataset(dataset_name):
     if dataset_name=="Iris":
         data=datasets.load_iris()
@@ -27,3 +28,21 @@ def get_dataset(dataset_name):
 x,y = get_dataset(dataset_name)  
 st.write("Shape of dataset", x.shape)
 st.write("number of classes", len(np.unique(y)))  
+
+# Add Sidebar with change value
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == "KNN":
+        K = st.sidebar.slider("K", 1, 15)
+        params["K"]=K
+    elif clf_name == "SVM":
+            C= st.sidebar.slider("C",0.01,10.0)
+            params["C"]=C
+    else:
+            max_depth= st.sidebar.slider("max_depth",2,15)
+            n_estimators=st.sidebar.slider("n_estimators",1,100)
+            params["max_depth"]=max_depth
+            params["n_estimators"]=n_estimators
+    return params
+
+add_parameter_ui(classifier_name)
